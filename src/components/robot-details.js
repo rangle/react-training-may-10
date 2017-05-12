bc21ee5b937cf716b84aeef3fc6fa90c582c1fd7
Varun Vachhar <varunvachhar@gmail.com>
2017-05-12 15:10:28 -0400

    wip

--- src/components/robot-details.js
+++ src/components/robot-details.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-export const RobotDetails = ({ match }) => (
-  <h1>Robot details for id: { match.params.id }</h1>
+export const RobotDetails = ({ robots, match }) => (
+  <div>
+    <h1>Robot details for id: { match.params.id }</h1>
+    <pre>{
+      JSON.stringify(
+        robots.find(robot => robot.id === Number(match.params.id)),
+        null, 2
+      )
+    }</pre>
+  </div>
 );
--- src/containers/details-container.js
+++ src/containers/details-container.js
@@ -2,21 +2,9 @@ import { connect } from 'react-redux';
 import { RobotDetails } from '../components/robot-details';
 import { createSelector } from 'reselect';
 
-const getRobotById = (robots, id) => {
-  return robots;
-}
-
-const selectRobotById = createSelector(
-  [
-    state => state.robots,
-    state => state.query,
-  ],
-  getRobotById,
-);
-
 const mapStateToProps = (state) => {
   return {
-    robot: selectRobotById(state),
+    robots: state.robots,
   }
 };
 
