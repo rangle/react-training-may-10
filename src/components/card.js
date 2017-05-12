7e21e968b88f27e430a51f7673c3bd2369538074
Varun Vachhar <varunvachhar@gmail.com>
2017-05-12 16:10:48 -0400

    tests

--- src/components/card.js
+++ src/components/card.js
@@ -5,7 +5,7 @@ import { Heading } from './heading';
 import { SubHeading } from './sub-heading';
 
 export const Card = ({ name, id, email }) => (
-  <article className="w5 mr3 bg-white br3 pa3 pa4-ns mv3 ba b--black-50">
+  <article className="w5 mr3 bg-light-green br3 pa3 pa4-ns mv3 ba b--black-50">
     <div className="tc">
       <Media
         src={`https://robohash.org/${id}`}
